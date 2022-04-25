86fb29615ea28f440e986b8ddb21d7af3055fe7d
claireli <claireli@umich.edu>
2022-04-25 13:11:51 -0400

    changes

--- finalproject.py
+++ finalproject.py
@@ -66,6 +66,63 @@ def get_data_to_databse(cur,conn,data_list):
     conn.commit()
 
 
+#VOO Calculation
+#SWITCH 410 TO THE {AVG_CLOSE} VARIABLE
+def find_average_volume_from_high_price_low_price(cur,conn):
+    #Finds the average Closing price for the voo
+    cur.execute("""SELECT AVG(close) FROM voo""")
+    avg_close = cur.fetchall()
+
+    #Finds the average volume for when price is below the average closing price
+    cur.execute("""SELECT AVG(volume) FROM voo WHERE close > 410""")
+    avg_volume_above = cur.fetchall()
+    print(avg_volume_above)
+
+    #Finds the average volume for when price is above the average closing price 
+    cur.execute("""SELECT AVG(volume) FROM voo WHERE close < 410""")
+    avg_volume_below = cur.fetchall()
+    print(avg_volume_below)
+    
+  
+#VOO boxplot
+def visualizations_voo(cur,conn):
+    #Finds the average Closing price for the voo
+    cur.execute("""SELECT AVG(close) FROM voo""")
+    avg_close = cur.fetchall()
+
+    #Finds the volume for when price is below the average closing price
+    cur.execute("""SELECT volume FROM voo WHERE close > 410""")
+    volume_above2 = cur.fetchall()
+
+    #Finds the volume for when price is above the average closing price 
+    cur.execute("""SELECT volume FROM voo WHERE close < 410""")
+    volume_below2 = cur.fetchall()
+
+    volume_above = []
+    volume_below = []
+    for i in volume_above2:
+        volume_above.append(i[0])
+    for i in volume_below2:
+        volume_below.append(i[0])
+    print(volume_above)
+    #Creates BoxPlot
+    data = [volume_below,volume_above]
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+               alpha=0.5)
+    ax1.set(
+        axisbelow=True,  # Hide the grid behind plot objects
+        xlabel='Volume Data',
+        ylabel='Volume Traded (in millions)',
+    )
+    labels = ['Volume below average closing price', 'Volume above average closing price']
+    ax1.set_xticklabels(np.repeat(labels,1),
+                    rotation=0, fontsize=8)
+    # Creating plot
+    plt.boxplot(data)
+    plt.title("Boxplot of volumes when price is below and above average closing price")
+    # show plot
+    plt.show()
 
 
 
@@ -86,9 +143,12 @@ def main():
     cur.execute(f"DROP TABLE IF EXISTS wsb")
     cur.execute(f"DROP TABLE IF EXISTS {wsbtoday}")
     cur.execute(f"DROP TABLE IF EXISTS {wsbmonthago}")
+    
     # #s&p
     # data_list = get_sstock()
     # get_data_to_databse(cur,conn,data_list)
+    print(find_average_volume_from_high_price_low_price(cur,conn))
+    print(visualizations_voo(cur,conn))
     
 if __name__ == "__main__":
     main()
