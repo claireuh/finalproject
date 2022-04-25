ab211106e64db06f3f2e0da202b25a7fcafb334a
claireli <claireli@umich.edu>
2022-04-24 23:15:47 -0400

    two tables for wsb

--- finalproject.py
+++ finalproject.py
@@ -19,18 +19,18 @@ def setUpDatabase(db_name):
 
 
 #wallstreetbets api
-def get_reddit_stocks():
-    url = 'https://tradestie.com/api/v1/apps/reddit'
+def get_reddit_stocks(date):
+    url = f'https://tradestie.com/api/v1/apps/reddit?date={date}'
     response = requests.get(url)
     data = response.text
     data_list = json.loads(data)
     return data_list[:25]
 
-def wsb_into_db(cur, conn, list):
-    cur.execute("DROP TABLE IF EXISTS wsb")
-    cur.execute("CREATE TABLE wsb (name TEXT, sentiment TEXT, sentimentscore INTEGER, comments INTEGER)")
+def wsb_into_db(cur, conn, list, name):
+    cur.execute(f"DROP TABLE IF EXISTS {name}")
+    cur.execute(f"CREATE TABLE {name} (name TEXT, sentiment TEXT, sentimentscore INTEGER, comments INTEGER)")
     for s in list:
-        cur.execute("INSERT INTO wsb (name,sentiment,sentimentscore,comments) VALUES (?,?,?,?)",(s['ticker'],s['sentiment'],s['sentiment_score'],s['no_of_comments']))
+        cur.execute(f"INSERT INTO {name} (name,sentiment,sentimentscore,comments) VALUES (?,?,?,?)",(s['ticker'],s['sentiment'],s['sentiment_score'],s['no_of_comments']))
     conn.commit()
 
 
@@ -43,14 +43,14 @@ def get_sstock():
     response = requests.get(url)
     data = response.text
     data_list = json.loads(data)
-    limited_data = {}
-    i = 0
-    for d in data_list:
-        if i < 25:
-            limited_data[d] = data_list[d]
-            i += 1
-    return limited_data
+    return data_list
 
+def get_sstock():
+    url = 'http://api.marketstack.com/v1/eod?access_key=5dbe879de68dbe57111390b991d08988&symbols=VOO&limit=25'
+    response = requests.get(url)
+    data = response.text
+    data_list = json.loads(data)
+    return data_list
 
 #Open
 #Close
@@ -60,9 +60,9 @@ def get_sstock():
 
 def get_data_to_databse(cur,conn,data_list):
     cur.execute("DROP TABLE IF EXISTS voo")
-    cur.execute("CREATE TABLE voo (open REAL, close REAL, volume INTEGER, date TEXT)")
+    cur.execute("CREATE TABLE voo (symbol TEXT, open REAL, close REAL, volume INTEGER, date TEXT)")
     for i in data_list['data']:
-        cur.execute("INSERT INTO voo (open,close,volume,date) VALUES (?,?,?,?)",(i['open'],i['close'],i['volume'],i['date']))
+        cur.execute("INSERT INTO voo (symbol,open,close,volume,date) VALUES (?,?,?,?,?)",(i['symbol'],i['open'],i['close'],i['volume'],i['date']))
     conn.commit()
 
 
@@ -78,13 +78,17 @@ def main():
     # SETUP DATABASE AND TABLE
     cur, conn = setUpDatabase('stocks.db')
    
-    #wsb
-    wsbdata = get_reddit_stocks()
-    wsb_into_db(cur, conn, wsbdata)
-
-    #s&p
-    data_list = get_sstock()
-    get_data_to_databse(cur,conn,data_list)
+    # #wsb
+    wsbtoday = get_reddit_stocks('2022-04-22')
+    wsbmonthago = get_reddit_stocks('2022-03-22')
+    wsb_into_db(cur, conn, wsbtoday, 'wsbtoday')
+    wsb_into_db(cur, conn, wsbmonthago, 'wsbmonthago')
+    cur.execute(f"DROP TABLE IF EXISTS wsb")
+    cur.execute(f"DROP TABLE IF EXISTS {wsbtoday}")
+    cur.execute(f"DROP TABLE IF EXISTS {wsbmonthago}")
+    # #s&p
+    # data_list = get_sstock()
+    # get_data_to_databse(cur,conn,data_list)
     
 if __name__ == "__main__":
     main()
