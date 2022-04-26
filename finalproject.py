da51f5be6f71b9b0f2949e0015b0183b2087c70d
claireli <claireli@umich.edu>
2022-04-25 21:31:05 -0400

    visualizations done

--- finalproject.py
+++ finalproject.py
@@ -59,48 +59,6 @@ def get_data_to_databse(cur,conn,data_list):
     conn.commit()
     
 
-  
-#VOO boxplot
-def visualizations_voo(cur,conn):
-    #Finds the average Closing price for the voo
-    cur.execute("""SELECT AVG(close) FROM voo""")
-    avg_close = cur.fetchall()
-
-    #Finds the volume for when price is below the average closing price
-    cur.execute(f"""SELECT volume FROM voo WHERE close > {avg_close} """)
-    volume_above2 = cur.fetchall()
-
-    #Finds the volume for when price is above the average closing price 
-    cur.execute(f"""SELECT volume FROM voo WHERE close < {avg_close} """)
-    volume_below2 = cur.fetchall()
-
-    volume_above = []
-    volume_below = []
-    for i in volume_above2:
-        volume_above.append(i[0])
-    for i in volume_below2:
-        volume_below.append(i[0])
-    print(volume_above)
-    #Creates BoxPlot
-    data = [volume_below,volume_above]
-    fig, ax1 = plt.subplots(figsize=(10, 6))
-    ax1.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
-               alpha=0.5)
-    ax1.set(
-        axisbelow=True,  # Hide the grid behind plot objects
-        xlabel='Volume Data',
-        ylabel='Volume Traded (in millions)',
-    )
-    labels = ['Volume below average closing price', 'Volume above average closing price']
-    ax1.set_xticklabels(np.repeat(labels,1),
-                    rotation=0, fontsize=8)
-    # Creating plot
-    plt.boxplot(data)
-    plt.title("Boxplot of volumes when price is below and above average closing price")
-    # show plot
-    plt.show()
-
-
 
 
 #analyst_rating
@@ -135,20 +93,19 @@ def put_data_in_database(cur, conn, response):
         cur.execute("INSERT OR IGNORE INTO hotstocks (rating,percentchangeday,percentchangemonth,symbol) VALUES (?,?,?,?)",(i['analyst_rating'], i['price_change_percent_1d'], i['price_change_percent_1m'], i['symbol']))
     conn.commit()
     
-
+#EXTRACTING DATA FROM DATABASE
 
 def wsbdata(cur, conn):
     cur.execute('SELECT wsbtoday.name, wsbmonthago.comments, wsbtoday.comments FROM wsbmonthago JOIN wsbtoday ON wsbmonthago.name = wsbtoday.name')
     comments = cur.fetchall()
-    print('wsb1')
-    print(comments)
+    # print('wsb1')
     return comments
 
 #getting the sentiment score of stocks today vs 1 month ago
 def wsbdata2(cur, conn):
     cur.execute('SELECT name, sentimentscore FROM wsbtoday ORDER BY sentimentscore DESC')
     bullish_lastmonth = cur.fetchall()
-    print("Stocks that were bullish 1 month ago in WSB")
+    # print("Stocks that were bullish 1 month ago in WSB")
     return bullish_lastmonth
 
 
@@ -156,28 +113,26 @@ def wsbdata2(cur, conn):
 def hotstockdata(cur, conn):
     cur.execute("""SELECT hotstocks.rating, hotstocks.percentchangemonth FROM hotstocks WHERE rating LIKE '%Buy%'""")
     data_buy = cur.fetchall()
-    print("hotstocks")
+    # print("hotstocks")
     return data_buy
 
 def voodata(cur, conn):
     cur.execute("""SELECT AVG(close) FROM voo""")
     avg_close = cur.fetchall()
     avg_close = avg_close[0][0]
-    print(avg_close)
 
     #Finds the volume for when price is below the average closing price
-    cur.execute(f"""SELECT AVG(volume) FROM voo WHERE close > {avg_close} """)
+    cur.execute(f"""SELECT volume FROM voo WHERE close > {avg_close} """)
     volume_above2 = cur.fetchall()
-    print(volume_above2)
 
     #Finds the volume for when price is above the average closing price 
-    cur.execute(f"""SELECT AVG(volume) FROM voo WHERE close < {avg_close} """)
+    cur.execute(f"""SELECT volume FROM voo WHERE close < {avg_close} """)
     volume_below2 = cur.fetchall()
-    print(volume_below2)
-    diff_vol = volume_above2[0][0] - volume_below2[0][0]
-    vollist = []
-    vollist.append(diff_vol)
-    return vollist
+    
+    combinedlist = []
+    combinedlist.append(volume_above2)
+    combinedlist.append(volume_below2)
+    return combinedlist
 
 #WRITING CSVs
 
@@ -214,8 +169,7 @@ def write_csv_hot_stocks(datalist, file_name):
 def wsb_sentimentchange(datalist, file_name):
     path = os.path.dirname(os.path.abspath(__file__))
     f = open(path + "/" + file_name, "w")
-    print("I'm here ---------------_**********")
-    print(datalist)
+    # print(datalist)
     f.write("The sentiment of WSB stocks today")
     f.write('\n')
     f.write("Ticker, Sentiment\n")
@@ -225,13 +179,13 @@ def wsb_sentimentchange(datalist, file_name):
         f.write('\n')
     f.close()
 
-def voo_volumedifference(datalist, file_name):
+def voo_volume(datalist, file_name):
     path = os.path.dirname(os.path.abspath(__file__))
     f = open(path + "/" + file_name, "w")
-    f.write("The difference in volumes between the average volume above the average closing price and the average volume below the average closing price")
-    f.write('\n')
-    for i in datalist:
-        f.write(str(i))
+    f.write("volumes when price is below and above average closing price\n")
+    f.write('Volumes above average,Volumes below average\n')
+    for i in range(len(datalist[0])):
+        f.write(str(datalist[0][i][0]) + "," + str(datalist[1][i][0]))
         f.write('\n')
     f.close()
 
@@ -239,28 +193,30 @@ def voo_volumedifference(datalist, file_name):
 
 #VISUALIZATIONS
 
-#comments visualization
-def data2vis(csvfile):
+#data 3 (comments) visualization
+def data3vis(csvfile):
+    f = open(os.path.abspath(os.path.join(os.path.dirname(__file__), csvfile)))
+    lines = f.readlines()
     labels = []
     month = []
     today = []
-    for row in csvfile[2:]:
-        val = row.split(",").strip()
-        labels.append(val[0])
-    labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-    men_means = [20, 34, 30, 35, 27]
-    women_means = [25, 32, 34, 20, 25]
+    for row in lines[2:]:
+        val = row.split(",")
+        labels.append(val[0].strip())
+        month.append(int(val[1].strip()))
+        today.append(int(val[2].strip()))
+    
 
     x = np.arange(len(labels))  # the label locations
     width = 0.35  # the width of the bars
 
     fig, ax = plt.subplots()
-    rects1 = ax.bar(x - width/2, men_means, width, label='Men')
-    rects2 = ax.bar(x + width/2, women_means, width, label='Women')
+    rects1 = ax.bar(x - width/2, month, width, label='1 Month ago')
+    rects2 = ax.bar(x + width/2, today, width, label='Today')
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Scores')
-    ax.set_title('Scores by group and gender')
+    ax.set_ylabel('Number of Comments')
+    ax.set_title('Number of Comments on WSB for Stocks One Month Ago vs Today')
     ax.set_xticks(x, labels)
     ax.legend()
 
@@ -272,45 +228,173 @@ def data2vis(csvfile):
     plt.show()
 
 
+#data 2 visualization barchart:
+def data2vis(csvfile):
+    f = open(os.path.abspath(os.path.join(os.path.dirname(__file__), csvfile)))
+    lines = f.readlines()
+
+    tickers = [] #x axis
+    sentiments = [] #y axis
+    for row in lines[2:]:
+        val = row.split(",")
+        tickers.append(val[0])
+        sentiments.append(float(val[1]))
+    plt.ylabel("Stock Ticker")
+    plt.xlabel("Stock Sentiment")
+    plt.xticks([-0.4,-0.2,0,0.2,0.4,0.6,0.8,1])
+    plt.title("Sentiment of Stocks on Wall Street Bets Today")
+    plt.barh(tickers, sentiments)
+    plt.tight_layout()
+    plt.show()
+
+#VOO boxplot
+def visualizations_voo(cur,conn):
+    #Finds the average Closing price for the voo
+    cur.execute("""SELECT AVG(close) FROM voo""")
+    avg_close = cur.fetchall()
+    avg_close = avg_close[0][0]
+
+    #Finds the volume for when price is below the average closing price
+    cur.execute(f"""SELECT volume FROM voo WHERE close > {avg_close} """)
+    volume_above2 = cur.fetchall()
+
+    #Finds the volume for when price is above the average closing price 
+    cur.execute(f"""SELECT volume FROM voo WHERE close < {avg_close} """)
+    volume_below2 = cur.fetchall()
+
+    volume_above = []
+    volume_below = []
+    for i in volume_above2:
+        volume_above.append(i[0])
+    for i in volume_below2:
+        volume_below.append(i[0])
+    #Creates BoxPlot
+    data = [volume_below,volume_above]
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+               alpha=0.5)
+    ax1.set(
+        axisbelow=True,  # Hide the grid behind plot objects
+        xlabel='Volume Data',
+        ylabel='Volume Traded (in millions)',
+    )
+    labels = ['Volume when price is below average closing price', 'Volume when price is above average closing price']
+    ax1.set_xticklabels(np.repeat(labels,1),
+                    rotation=0, fontsize=8)
+    # Creating plot
+    plt.boxplot(data)
+    plt.title("Boxplot of volumes when price is below and above average closing price")
+    # show plot
+    plt.show()
+
+
+
+# Hot Stocks Visualizatoin
+def hot_stock_vis(cur,conn):
+    cur.execute("""SELECT hotstocks.rating, hotstocks.percentchangemonth FROM hotstocks WHERE rating LIKE '%Buy%'""")
+    data_buy = cur.fetchall()
+    data_buy_change = []
+    for i in data_buy:
+        data_buy_change.append(float(i[1]))
+    cur.execute("""SELECT hotstocks.rating, hotstocks.percentchangemonth FROM hotstocks WHERE rating LIKE '%Hold%'""")
+    data_hold = cur.fetchall()
+    data_hold_change = []
+    for i in data_hold:
+        data_hold_change.append(float(i[1]))
+
+     #Creates BoxPlot
+    data = [data_buy_change,data_hold_change]
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+                alpha=0.5)
+    ax1.set(
+            axisbelow=True,  # Hide the grid behind plot objects
+            xlabel='Analysis rating',
+            ylabel='Percent Change',
+        )
+    labels = ['Percent change when analyst say to buy', 'Percent change when analysis say to hold']
+    ax1.set_xticklabels(np.repeat(labels,1),
+                        rotation=0, fontsize=8)
+    # Creating plot
+    plt.boxplot(data)
+    plt.title("Boxplot of percent change in one month based off of analyst rating (penny stocks)")
+    # show plot
+    plt.show()
+
+# Hot Stocks Visualizatoin2
+def hot_stock_vis2(cur,conn):
+    cur.execute("""SELECT hotstocks.rating, hotstocks.percentchangeday FROM hotstocks WHERE rating LIKE '%Buy%'""")
+    data_buy = cur.fetchall()
+    data_buy_change = []
+    for i in data_buy:
+        data_buy_change.append(float(i[1]))
+    cur.execute("""SELECT hotstocks.rating, hotstocks.percentchangeday FROM hotstocks WHERE rating LIKE '%Hold%'""")
+    data_hold = cur.fetchall()
+    data_hold_change = []
+    for i in data_hold:
+        data_hold_change.append(float(i[1]))
+
+     #Creates BoxPlot
+    data = [data_buy_change,data_hold_change]
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
+                alpha=0.5)
+    ax1.set(
+            axisbelow=True,  # Hide the grid behind plot objects
+            xlabel='Analysis rating',
+            ylabel='Percent Change',
+        )
+    labels = ['Percent change when analyst say to buy', 'Percent change when analysis say to hold']
+    ax1.set_xticklabels(np.repeat(labels,1),
+                        rotation=0, fontsize=8)
+    # Creating plot
+    plt.boxplot(data)
+    plt.title("Boxplot of percent change in one day based off of analyst rating (penny stocks)")
+    # show plot
+    plt.show()
+
+#MAIN
 def main():
     # SETUP DATABASE AND TABLE
     cur, conn = setUpDatabase('stocks.db')
    
-    #wsb
+    #wsb data
     wsbtoday = get_reddit_stocks('2022-04-22')
     wsbmonthago = get_reddit_stocks('2022-03-22')
     wsb_into_db(cur, conn, wsbtoday, 'wsbtoday')
     wsb_into_db(cur, conn, wsbmonthago, 'wsbmonthago')
     
     
-    # #s&p
+    # voo data
     # data_list = get_sstock()
     # get_data_to_databse(cur,conn,data_list)
     # print(find_average_volume_from_high_price_low_price(cur,conn))
     # print(visualizations_voo(cur,conn))
 
     #hotstocks data
-
-    #getting the data
     cur.execute("DROP TABLE IF EXISTS hotstocks")
     response = hotstocks()
-    # print(response)
     put_data_in_database(cur, conn, response)
 
 
     #calling the data
     data1 = hotstockdata(cur,conn)
     data2 = wsbdata2(cur,conn)
-    #writing the wsbdata2 to a csv file that contains the tickers of stocks that were bullish 1 month ago
     data3 = wsbdata(cur, conn)
     data4 = voodata(cur,conn)
+    
+    #writing data to a csv file
     write_csv(data3, 'data3.csv')
     wsb_sentimentchange(data2, 'data2.csv')
     write_csv_hot_stocks(data1, 'data1.csv')
-    voo_volumedifference(data4, 'data4.csv')
+    voo_volume(data4, 'data4.csv')
 
     #calling the visualizations
-    
+    data3vis('data3.csv')
+    visualizations_voo(cur,conn)
+    hot_stock_vis(cur,conn)
+    data2vis('data2.csv')
+    hot_stock_vis2(cur,conn)
 
 
 if __name__ == "__main__":
